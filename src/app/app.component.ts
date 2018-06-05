0ed8784d3445b7293bc4b23c5cead8bda6e22483
FranckAchard <36164145+FranckAchard@users.noreply.github.com>
2018-06-05 12:24:20 +0200

    sav exos 1 et 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,10 @@ export class AppComponent {
   user = {
     name: 'Toto',
     JobTitle: 'Suppôt de satan',
-    image: 'https://cdn.pixabay.com/photo/2017/01/31/14/22/dino-2024517_1280.png'
+    // image: 'https://cdn.pixabay.com/photo/2017/01/31/14/22/dino-2024517_1280.png',
+    image: 'https://bbs.ichunqiu.com/data/attachment/forum/201711/24/181455vq7d28itd0772tj0.gif',
+    color: 'blue',
+    urgent: false
   };
 
   setName(text) {
