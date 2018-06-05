12efd4070e2e597612b5b5306557c133a108f5ee
FranckAchard <36164145+FranckAchard@users.noreply.github.com>
2018-06-05 09:45:03 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Business Card';
+  user = {
+    name: 'Toto',
+    JobTitle: 'Suppôt de satan',
+    image: 'https://cdn.pixabay.com/photo/2017/01/31/14/22/dino-2024517_1280.png'
+  };
+
+  setName(text) {
+    this.user.name = text;
+  }
+  setJob(text) {
+    this.user.JobTitle = text;
+  }
+  setImage(text) {
+    this.user.image = text;
+  }
 }
